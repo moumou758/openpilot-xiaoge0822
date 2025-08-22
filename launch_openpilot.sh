63e52a72dad5fa6c9c6b762ccfcd8cca71bcc001
moumou758 <moumou758@163.com>
2025-08-22 22:56:49 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-if [[ "$(cat /data/params/d/EnableConnect)" == "2" ]]; then
-  export API_HOST="https://api.carrotpilot.app"
-  export ATHENA_HOST="wss://athena.carrotpilot.app"
-fi
+export API_HOST=https://api.konik.ai
+export ATHENA_HOST=wss://athena.konik.ai
+export MAPS_HOST=https://api.konik.ai/maps
 exec ./launch_chffrplus.sh
