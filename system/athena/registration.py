3109fc513352a48c2b6c9c78ab211a18b796266f
moumou758 <moumou758@163.com>
2025-08-22 10:27:42 +0800

    Update registration.py

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -34,7 +34,7 @@ def register(show_spinner=False) -> str | None:
   params = Params()
 
 
-  #return UNREGISTERED_DONGLE_ID  # for c3lite, clone
+  return UNREGISTERED_DONGLE_ID  # for c3lite, clone
   dongle_id: str | None = params.get("DongleId", encoding='utf8')
   if dongle_id is None and Path(Paths.persist_root()+"/comma/dongle_id").is_file():
     # not all devices will have this; added early in comma 3X production (2/28/24)
@@ -58,8 +58,8 @@ def register(show_spinner=False) -> str | None:
     # Block until we get the imei
     serial = HARDWARE.get_serial()
     start_time = time.monotonic()
-    imei1: str | None = None
-    imei2: str | None = None
+    imei1: str | None = "123456789012345"
+    imei2: str | None = "987654321098765"
     while imei1 is None and imei2 is None:
       try:
         imei1, imei2 = HARDWARE.get_imei(0), HARDWARE.get_imei(1)
@@ -99,7 +99,7 @@ def register(show_spinner=False) -> str | None:
 
   if dongle_id:
     params.put("DongleId", dongle_id)
-    set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
+    #set_offroad_alert("Offroad_UnofficialHardware", (dongle_id == UNREGISTERED_DONGLE_ID) and not PC)
   return dongle_id
 
 
