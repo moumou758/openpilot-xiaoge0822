9bccdca36eab9f6b616824c31033a55fb3386e8f
ajouatom <ajouatom@gmail.com>
2025-09-01 18:33:24 +0900

    fix radar..

--- opendbc_repo/opendbc/car/hyundai/radar_interface.py
+++ opendbc_repo/opendbc/car/hyundai/radar_interface.py
@@ -8,6 +8,7 @@ from openpilot.common.params import Params
 from opendbc.car.hyundai.hyundaicanfd import CanBus
 from openpilot.common.filter_simple import MyMovingAverage
 
+SCC_TID = 0
 RADAR_START_ADDR = 0x500
 RADAR_MSG_COUNT = 32
 RADAR_START_ADDR_CANFD1 = 0x210
@@ -49,7 +50,7 @@ def get_radar_can_parser_scc(CP):
 class RadarInterface(RadarInterfaceBase):
   def __init__(self, CP):
     super().__init__(CP)
-    self.updated_messages = set()
+    
     self.canfd = True if CP.flags & HyundaiFlags.CANFD else False
     self.radar_group1 = False
     if self.canfd:
@@ -63,60 +64,76 @@ class RadarInterface(RadarInterfaceBase):
     else:
       self.radar_start_addr = RADAR_START_ADDR
       self.radar_msg_count = RADAR_MSG_COUNT
-    self.trigger_msg = self.radar_start_addr + self.radar_msg_count - 1
+      
+    self.params = Params()
+    self.radar_tracks = self.params.get_int("EnableRadarTracks") >= 1
+    self.updated_tracks = set()
+    self.updated_scc = set()
+    self.rcp_tracks = get_radar_can_parser(CP, self.radar_tracks, self.radar_start_addr, self.radar_msg_count)
+    self.rcp_scc = get_radar_can_parser_scc(CP)
+    self.trigger_msg_scc = 416 if self.canfd else 0x420
+
+    self.trigger_msg_tracks = self.radar_start_addr + self.radar_msg_count - 1
     self.track_id = 0
 
     self.radar_off_can = CP.radarUnavailable
 
-    self.params = Params()
-    self.radar_tracks = self.params.get_int("EnableRadarTracks") >= 1
-    self.rcp = get_radar_can_parser(CP, self.radar_tracks, self.radar_start_addr, self.radar_msg_count)
-
-    if not self.radar_tracks:
-      self.rcp = get_radar_can_parser_scc(CP)
-      self.trigger_msg = 416 if self.canfd else 0x420
-
-    # 50Hz (SCC), 20Hz (RadarTracks)
-    self.vLead_filter = MyMovingAverage(13) # for SCC radar 0.1 unit
     self.vRel_last = 0
     self.dRel_last = 0
 
-    radar_msg_count = self.radar_msg_count * 2 if self.radar_group1 else self.radar_msg_count
-    for track_id in range(radar_msg_count):
-      self.pts[track_id] = structs.RadarData.RadarPoint()
-      self.pts[track_id].measured = False
-      self.pts[track_id].trackId = track_id
+    # Initialize pts
+    total_tracks = self.radar_msg_count * ( 2 if self.radar_group1 else 1)
+    for track_id in range(total_tracks):
+      t_id = track_id + 32
+      self.pts[t_id] = structs.RadarData.RadarPoint()
+      self.pts[t_id].measured = False
+      self.pts[t_id].trackId = t_id
+
+    self.pts[SCC_TID] = structs.RadarData.RadarPoint()
+    self.pts[SCC_TID].trackId = SCC_TID
+
+    self.frame = 0
 
 
   def update(self, can_strings):
-    if self.radar_off_can or (self.rcp is None):
+    self.frame += 1
+    if self.radar_off_can or (self.rcp_tracks is None and self.rcp_scc is None):
       return super().update(None)
 
-    vls = self.rcp.update(can_strings)
-    self.updated_messages.update(vls)
+    if self.rcp_scc is not None:
+      vls_s = self.rcp_scc.update(can_strings)
+      self.updated_scc.update(vls_s)
+      if not self.radar_tracks and self.frame % 5 == 0:
+        self._update_scc(self.updated_scc)
+        self.updated_scc.clear()
+        ret = structs.RadarData()
+        if not self.rcp_scc.can_valid:
+          ret.errors.canError = True
+        ret.points = list(self.pts.values())
+        return ret
+    if self.radar_tracks and self.rcp_tracks is not None:
+      vls_t = self.rcp_tracks.update(can_strings)
+      self.updated_tracks.update(vls_t)
+      if self.trigger_msg_tracks in self.updated_tracks:
+        self._update(self.updated_tracks)
+        self._update_scc(self.updated_scc)
+        self.updated_scc.clear()
+        self.updated_tracks.clear()
+        ret = structs.RadarData()
+        if not self.rcp_tracks.can_valid:
+          ret.errors.canError = True
+        ret.points = list(self.pts.values())
+        return ret
 
-    if self.trigger_msg not in self.updated_messages:
-      return None
-
-    rr = self._update(self.updated_messages) if self.radar_tracks else self._update_scc(self.updated_messages)
-    self.updated_messages.clear()
-
-    return rr
+    return None      
 
   def _update(self, updated_messages):
-    ret = structs.RadarData()
-    if self.rcp is None:
-      return ret
 
-    if not self.rcp.can_valid:
-      ret.errors.canError = True
-
-    self.track_id = 0
+    t_id = 32
     for addr in range(self.radar_start_addr, self.radar_start_addr + self.radar_msg_count):
 
-      msg = self.rcp.vl[f"RADAR_TRACK_{addr:x}"]
+      msg = self.rcp_tracks.vl[f"RADAR_TRACK_{addr:x}"]
 
-      t_id = self.track_id
       if self.radar_group1:
         valid = msg['VALID_CNT1'] > 10
       elif self.canfd:
@@ -155,13 +172,11 @@ class RadarInterface(RadarInterfaceBase):
         self.pts[t_id].aRel = msg['REL_ACCEL']
         self.pts[t_id].yvRel = 0.0
 
-      self.track_id += 1
+      t_id += 1
     # radar group1은 하나의 msg에 2개의 레이더가 들어있음.
     if self.radar_group1:
       for addr in range(self.radar_start_addr, self.radar_start_addr + self.radar_msg_count):
-        msg = self.rcp.vl[f"RADAR_TRACK_{addr:x}"]
-
-        t_id = self.track_id
+        msg = self.rcp_tracks.vl[f"RADAR_TRACK_{addr:x}"]
 
         valid = msg['VALID_CNT2'] > 10
         self.pts[t_id].measured = valid
@@ -180,23 +195,11 @@ class RadarInterface(RadarInterfaceBase):
           self.pts[t_id].aRel = msg['REL_ACCEL2']
           self.pts[t_id].yvRel = msg['LAT_SPEED2']
 
-        self.track_id += 1
-      
-    ret.points = list(self.pts.values())
-    return ret
-
+        t_id += 1
 
   def _update_scc(self, updated_messages):
-    ret = structs.RadarData()
-    if self.rcp is None:
-      return ret
-
-    if not self.rcp.can_valid:
-      ret.errors.canError = True
-
-    cpt = self.rcp.vl
-    self.track_id = 0
-    t_id = self.track_id
+    cpt = self.rcp_scc.vl
+    t_id = SCC_TID
     if self.canfd:
       dRel = cpt["SCC_CONTROL"]['ACC_ObjDist']
       vRel = cpt["SCC_CONTROL"]['ACC_ObjRelSpd']
@@ -205,7 +208,6 @@ class RadarInterface(RadarInterfaceBase):
       valid = 0 < dRel < 150 and not new_pts #cpt["SCC_CONTROL"]['OBJ_STATUS'] and dRel < 150
       self.pts[t_id].measured = valid
       if not valid:
-        self.vLead_filter.set_all(vLead)
         self.pts[t_id].dRel = 0
         self.pts[t_id].yRel = 0
         self.pts[t_id].vRel = 0
@@ -216,7 +218,7 @@ class RadarInterface(RadarInterfaceBase):
         self.pts[t_id].dRel = dRel
         self.pts[t_id].yRel = 0
         self.pts[t_id].vRel = vRel
-        self.pts[t_id].vLead = self.vLead_filter.process(vLead)
+        self.pts[t_id].vLead = vLead
         self.pts[t_id].aRel = 0 #float('nan')
         self.pts[t_id].yvRel = 0 #float('nan')
     else:
@@ -227,7 +229,6 @@ class RadarInterface(RadarInterfaceBase):
       valid = cpt["SCC11"]['ACC_ObjStatus'] and dRel < 150 and not new_pts
       self.pts[t_id].measured = valid
       if not valid:
-        self.vLead_filter.set_all(vLead)
         self.pts[t_id].dRel = 0
         self.pts[t_id].yRel = 0
         self.pts[t_id].vRel = 0
@@ -238,11 +239,9 @@ class RadarInterface(RadarInterfaceBase):
         self.pts[t_id].dRel = dRel
         self.pts[t_id].yRel = -cpt["SCC11"]['ACC_ObjLatPos']  # in car frame's y axis, left is negative
         self.pts[t_id].vRel = vRel
-        self.pts[t_id].vLead = self.vLead_filter.process(vLead)
+        self.pts[t_id].vLead = vLead
         self.pts[t_id].aRel = 0 #float('nan')
         self.pts[t_id].yvRel = 0 #float('nan')
 
     self.dRel_last = dRel
     self.vRel_last = vRel
-    ret.points = list(self.pts.values())
-    return ret
